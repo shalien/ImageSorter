4abe13246f52f8c0245e4129cb0e92c4d64f9238
Ophélien Duparc <ophelien.duparc@gmail.com>
2019-03-28 17:50:20 +0100

    Added crypto and most logic

--- main.dart
+++ main.dart
@@ -1,13 +1,101 @@
-import 'dart:io' show Platform, File, Directory;
 import 'dart:core';
+import 'dart:io';
 import 'package:image/image.dart';
 
-void main() {
+Future<int> main() async {
 
-  Map<String, String> envVars = Platform.environment;
+  var homePath = Platform.environment['HOMEPATH'];
+  var homePathUri = Uri.directory(homePath);
 
-  final String HOME_PATH = envVars['HOMEPATH'];
+  var onedriveDirectory;
 
-  print(HOME_PATH);
+  try {
+    onedriveDirectory = await findOnedriveDirectory(homePathUri);
+  } catch(ex) {
+    print(ex);
+    return 2;
+  }
+
+  if(onedriveDirectory is !Directory) {
+    print("Missing Onedrive Directory");
+    return 0;
+  }
+
+  var wallpaperDirectory;
+
+  try {
+    wallpaperDirectory = await findWallpaperDirectory(onedriveDirectory.uri);
+  } catch (ex) {
+    print(ex);
+    return 2;
+  }
+
+  if(wallpaperDirectory is !Directory) {
+    print("Missing Onedrive Directory");
+    return 0;
+  }
+
+  var wallpaperFiles = await getAllImagesInWallpaperDirectory((wallpaperDirectory as FileSystemEntity).uri);
+
+  for(var file in wallpaperFiles) {
+
+    var image;
+
+    try {
+      image = decodeImage(File.fromUri(file.uri).readAsBytesSync());
+    } catch (ex) {
+      print("Unable to load file ${filename(file.path)}");
+      File.fromUri(file.uri).deleteSync();
+      continue;
+    }
+
+    print("${filename(file.path)} ${is16by9(image)}");
+  }
 
 }
+
+
+Future<FileSystemEntity> findOnedriveDirectory(Uri uri) async {
+
+  var onedriveDirectory = await Directory.fromUri(uri)
+      .list()
+      .firstWhere((fse) =>
+  fse.path
+      .split(Platform.pathSeparator)
+      .last
+      .toLowerCase() == 'onedrive');
+
+  return onedriveDirectory;
+
+}
+
+Future<FileSystemEntity> findWallpaperDirectory(Uri uri) async {
+
+  var wallpaperDirectory = await Directory.fromUri(uri)
+      .list()
+      .firstWhere((fse) =>
+  fse.path
+      .split(Platform.pathSeparator)
+      .last
+      .toLowerCase() == 'wallpaper');
+
+  return wallpaperDirectory;
+
+}
+
+Future<List<FileSystemEntity>> getAllImagesInWallpaperDirectory(Uri uri) async {
+
+  var wallpaperList = await Directory.fromUri(uri)
+      .list()
+      .where((fse) => fse.statSync().type == FileSystemEntityType.file)
+      .toList();
+
+  return wallpaperList;
+}
+
+
+bool is16by9(Image image) {
+  return image.width % 16 == 0 && image.height % 9 == 0;
+}
+
+String filename(String path) => path.split(Platform.pathSeparator).last;
